aacf728011d8aeb05ea1ce9d89b59d07787802de
Egran-Andr <mlgrainbowfrog2003@gmail.com>
2023-08-02 21:54:00 +0300

    Created firebase user update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from "./pages/HomePage"
 import "./style.scss";
 function App() {
     return (
-        <HomePage />
+        <RegistrationPage />
   );
 }
 
--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,14 +1,33 @@
 import React from "react"
 import Add from "../img/addAvatar.png"
-
+import {createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import { useState } from "react";
 const RegistrationPage = () => {
+    const [err, setErr] = useState(false);
+    const handleSubmit = async (e) => {
 
+        e.preventDefault();
+        const displayName = e.target[0].value;
+        const email = e.target[1].value;
+        const password = e.target[2].value;
+        const file = e.target[3].files[0];
+
+        try {
+            const res = await createUserWithEmailAndPassword(auth, email, password);
+        }
+        catch (err) {
+            setErr(true);
+        }
+
+
+    }
     return (
         <div className="formContainer">
             <div className="formWrapper">
                 <span className="logo">Chat</span>
                 <span className="tittle">Register</span>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input type="text" placeholder="display name"></input>
                     <input type="email" placeholder="Email"></input>
                     <input type="password" placeholder="Password"></input>
@@ -18,6 +37,7 @@ const RegistrationPage = () => {
                         <span>Add avatar</span>
                     </label>
                     <button>Sign up</button>
+                    {err && <span>Something went wrong</span>}
                 </form>
                 <p>Already have an account? Login </p>
             </div>
