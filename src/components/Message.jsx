12d27f58fc36130b5f9a6d601729937abf6f2cf7
Egran-Andr <mlgrainbowfrog2003@gmail.com>
2023-08-04 20:00:26 +0300

    Realtime message show/input

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useContext, useEffect, useRef } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
-const Message = () => {
+const Message = ({ message }) => {
+    const { currentUser } = useContext(AuthContext);
+    const { data } = useContext(ChatContext);
+
+    const ref = useRef();
+
+    useEffect(() => {
+        ref.current?.scrollIntoView({ behavior: "smooth" });
+    }, [message]);
 
     return (
-        <div className="message owner">
+        <div
+            ref={ref}
+            className={`message ${message.senderId === currentUser.uid && "owner"}`}
+        >
             <div className="messageInfo">
-                <img src="https://avatars.dzeninfra.ru/get-zen_doc/34175/pub_5cea2361585c2f00b5c9cb0b_5cea310a752e5b00b25b9c01/scale_1200" alt=""></img>
-                <span>time</span>
+                <img
+                    src={
+                        message.senderId === currentUser.uid
+                            ? currentUser.photoURL
+                            : data.user.photoURL
+                    }
+                    alt=""
+                />
+                <span>just now</span>
             </div>
             <div className="messageContent">
-                <p>Test</p>
+                <p>{message.text}</p>
+                {message.img && <img src={message.img} alt="" />}
             </div>
         </div>
     );
 };
-export default Message
+
+export default Message;
